54c04b9abd0fed06571a482cca003798194ea937
Sajid16 <sajid.mahboob16@gmail.com>
2025-02-27 00:38:36 +0600

    completed the happy path for create product

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,30 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductHandler
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create product model from the command
+            // save into database
+            // return the saved product GUID id
+
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+                Name = command.Name
+            };
+
+            return new CreateProductResult(product.Id);
+        }
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,10 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+});
+
+builder.Services.AddCarter();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 
 app.MapGet("/", () => "Hello Catalog API!");
+
+app.MapCarter();
 
 app.Run();
