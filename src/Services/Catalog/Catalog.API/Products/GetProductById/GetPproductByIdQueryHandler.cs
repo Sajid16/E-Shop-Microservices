5405eeb3056b169f6d95624cedbbaaa1d5ea7dbf
Sajid16 <sajid.mahboob16@gmail.com>
2025-03-08 17:18:25 +0600

    get product by id endpoint done

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetPproductByIdQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid ProductId) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    internal class GetPproductByIdQueryHandler(IDocumentSession documentSession) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            var product = await documentSession.LoadAsync<Product>(query.ProductId, cancellationToken);
+            if (product is null) throw new ProductNotFoundException();
+            return new GetProductByIdResult(product);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,49 @@
+﻿using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductById
+{
+    // if i want to make the endpoint post then i need the request model
+    //public record GetProductByIdRequest(Guid ProductId);
+    public record GetProductByIdResponse(Product Product);
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            //app.MapPost("/product",
+            //async (GetProductByIdRequest request, ISender sender) =>
+            //{
+            //    var query = request.Adapt<GetProductByIdQuery>();
+
+            //    var result = await sender.Send(query);
+
+            //    var response = result.Adapt<GetProductByIdResponse>();
+
+            //    return Results.Ok(response);
+
+            //})
+            //.WithName("GetProductById")
+            //.Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            //.ProducesProblem(StatusCodes.Status400BadRequest)
+            //.WithSummary("Get Product By Id")
+            //.WithDescription("Get Product By Id");
+            
+            app.MapGet("/product/{productId}",
+            async (Guid productId, ISender sender) =>
+            {
+                var query = new GetProductByIdQuery(productId);
+
+                var result = await sender.Send(query);
+
+                var response = result.Adapt<GetProductByIdResponse>();
+
+                return Results.Ok(response);
+
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Id")
+            .WithDescription("Get Product By Id");
+        }
+    }
+}
